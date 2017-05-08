f71dccaeaa183fb84fd250f9c37d31231bf75692
tenzin <tenzin.jeunesse@gmail.com>
2017-05-08 19:28:15 +0000

    Added and change file in form

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,5 +4,23 @@ class MessagesController < ApplicationController
         
     end
     
+    def new
+        @message = Message.new
+    end
     
+    def create
+        @message = Message.new()
+        
+        if @message.save
+            redirect_to root_path
+        else
+            render 'new'
+        end
+    end
+    
+    private
+    
+    def message_params
+        params.require(:message).permit(:title, :description)
+    end
 end
